6d8132ecd24ea34c6e2bfc4e864a3325342ce8d4
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-22 20:00:57 +0100

    add vue logic

--- client/script.js
+++ client/script.js
@@ -1,7 +1,26 @@
 import Vue from 'vue';
+import axios from 'axios';
+
 
 const app = Vue.createApp({
+    data() {
+        return {
+            todos: []
+        };
+    },
+    mounted() {
 
+        axios.get('http://localhost/boolean/php-todo-list-json/server/todo.php')
+            .then(response => {
+
+                this.todos = response.data;
+            })
+            .catch(error => {
+
+                console.error('Errore durante la richiesta dei dati dei ToDo:', error);
+            });
+    }
 });
 
+// Monta l'app Vue sull'elemento con id "app"
 app.mount('#app');
